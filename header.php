a2f8f1813e6acd5668dbd05ec8169e281c4fff53
Cigarent <xiaojunh1996@hotmail.com>
2016-08-08 04:50:11 -0500

    dynamic generate nav bar link

--- header.php
+++ header.php
@@ -1,30 +1,51 @@
-<div class="header">
-	<div class="logo"></div>
-	<div class="links">
-		<a href="/">Home</a>
-		<a href="location.php">Location</a>
-		<a href="rush.php">Rush</a>
-		<a href="calendar.php">Calendar</a>
-		<a href="echoes.php">Newsletter</a>
-	</div>
-	<div class="menu-link">Menu&#9660;</div>
-	<div class="menu-mobile">
-		<ul>
-			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
-				<a href="location.php">Location</a>
-			</li>
-			<li>
-				<a href="rush.php">Rush</a>
-			</li>
-			<li>
-				<a href="calendar.php">Calendar</a>
-			</li>
-			<li>
-				<a href="echoes.php">Newsletter</a>
-			</li>
-		</ul>
-	</div>
-</div>
+<?php
+
+function printHead($home){
+
+$hrefList=array("index.php","about.php","rush.php","contact.php");
+$hrefActive =array("","","","");
+
+if($home==0){
+	$hrefList=array("#page-top","#about","#rush","#contact");
+}
+else{
+	$hrefActive[$home]="class=active";
+}
+
+$html=<<<HTML
+	<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+			<div class="container">
+					<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+									Menu <i class="fa fa-bars"></i>
+							</button>
+							<a class="navbar-brand page-scroll" href="{$hrefList[0]}">
+									<img src="images/triangle.jpg" height="50">
+							</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+							<ul class="nav navbar-nav">
+									<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+									<li class="hidden"><a href="#page-top"></a>
+									</li>
+									<li {$hrefActive[1]}>
+											<a class="page-scroll" href="{$hrefList[1]}">About</a>
+									</li>
+									<li {$hrefActive[2]}>
+											<a class="page-scroll" href="{$hrefList[2]}">Rush</a>
+									</li>
+									<li {$hrefActive[3]}>
+											<a class="page-scroll" href="{$hrefList[3]}">Contact</a>
+									</li>
+							</ul>
+					</div>
+					<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container -->
+	</nav>
+HTML;
+print $html;
+}
+?>
--- includes.php
+++ includes.php
@@ -1,7 +1,28 @@
-		<meta name="viewport" content="width=device-width" />
-		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-		<script src="js/script.js"></script>
-		<script src="js/bootstrap.js"></script>
-		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-		<link rel="stylesheet" type="text/css" href="css/index.css" />
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+		<head>
+
+		    <meta charset="utf-8">
+		    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		    <meta name="viewport" content="width=device-width, initial-scale=1">
+		    <meta name="description" content="Triangle Fraternity Wisconsin Chapter">
+		    <meta name="author" content="Xiao He">
+
+		    <title>Triangle Fraternity - UW Madison</title>
+				<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+		    <!-- Bootstrap Core CSS -->
+		    <link href="res/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+		    <!-- Custom Fonts -->
+		    <link href="res/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+		    <!-- Theme CSS -->
+		    <link href="css/main.css" rel="stylesheet">
+
+		    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		    <!--[if lt IE 9]>
+		        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		    <![endif]-->
+
+		</head>
