930f366f19b3ffb3f3a9f06672e6f173492aff70
Cigarent <xiaojunh1996@hotmail.com>
2016-08-08 02:46:23 -0500

    change header file

--- includes.php
+++ includes.php
@@ -1,5 +1,7 @@
 		<meta name="viewport" content="width=device-width" />
 		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
 		<script src="js/script.js"></script>
+		<script src="js/bootstrap.js"></script>
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="css/index.css" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
