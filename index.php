e794c5815fcc4eaa9865544f47474f2b682aee70
Cigarent <xiaojunh1996@hotmail.com>
2016-08-08 04:52:03 -0500

    index page all done

--- index.php
+++ index.php
@@ -1,38 +1,116 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
-	<head>
-		<title>Triangle UW-Madison</title>
-		
-		<?php include("includes.php"); ?>
-		
-	</head>
+<?php include("includes.php"); ?>
 
-	<body align="center">
-		<?php include("header.php"); ?>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
 
-		<div id="content" align="center">
+    <!-- Navigation -->
+    <?php include("header.php");
+          printHead(0);?>
 
-		 <h1>SUCCEED ACADEMICALLY</h1>
-		 <p>Triangle is serious about scholarship. Preparing yourself for your future career and achieving academic success are your primary goals at the University of Wisconsin-Madison.</p>
 
-		 <h1>LEARN TO BECOME A LEADER</h1>
-		 <p>Triangle provides an environment that fosters personal growth and professional success and provides the necessary skills to become an effective leader.</p>
-		 
-		 <h1>UTILIZE LOCAL AND NATIONAL JOB CONNECTIONS</h1>
-		 <p>Triangle's 29 nationwide chapters, 25,000 alumni and more than 100 years of history contribute to our rich traditions. Our national list-serves and message boards allow our alumni to share openings at their companies, in some cases before the jobs are made known to the general public.</p>
-		 
-		 <h1>SOCIALIZE</h1>
-		 <p>Social and athletic participation provides an enhancement to your busy and challenging college curriculum. Triangle is invited and has access to many events, including Homecoming, Greek Week/All Campus Party, formals and more.</p>
-		 
-		 <h1>DEVELOP LIFE LONG FRIENDSHIPS</h1>
-		 <p>Some of your brothers will be your closest friends because you shared experiences, worked together toward common goals and celebrated achievements during your time in school.</p>
-		 
-		</div>
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 text-background-intro">
+                      <h1 class="brand-heading">TRIANGLE﻿ FRATERNITY</h1>
+                      <p class="intro-text">A FRATERNITY OF ENGINEERS, ARCHITECTS, AND SCIENTISTS</p>
+                      <a href="#about" class="btn btn-circle page-scroll">
+                          <i class="fa fa-angle-double-down animated"></i>
+                      </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 
-		<?php include("contact.php"); ?>
+    <!-- About Section -->
+    <section id="about" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>About Triangle</h2>
+                <p>Triangle is a social fraternity built upon the ideals of brotherhood and friendships.</p>
+                <hr id="rush-hr">
+                <p>Triangle brothers are engineers, architects, and scientists who hold themselves to high moral standards
+                and strive to be the best men they can be in order build a better world for tomorrow.</p>
+                <a href="about.php" class="btn btn-default btn-lg">Learn More</a>
+            </div>
+        </div>
+    </section>
 
-	</body>
+    <!-- Download Section -->
+    <section id="rush" class="content-section text-center">
+        <div class="rush-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Rush Triangle</h2>
+                    <p>Triangle Fraternity is now accepting applications for membership for the Fall 2016 academic term. If you are looking to get involved on campus, serve the community, and develop effective leadership and professional networking experience, this is your opportunity to do so.</p>
+                    <a href="#" class="btn btn-default btn-lg">Visit Rush Page</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h2>Contact Us</h2>
+                <div class="row">
+                  <div class="col-lg-4 col-lg-offset-2">
+                    <p>Recruitment<br><em>Stephen McManus</em></p>
+                    <p>Phone: 843-709-4585<br><a href="mailto:smcmanus2@wisc.edu">smcmanus2@wisc.edu</a></p>
+                  </div>
+                  <div class="col-lg-4">
+                    <p>President<br><em>Kevin Clark</em></p>
+                    <p>Phone: 414-793-4032<br><a href="mailto:kwclark@wisc.edu">kwclark@wisc.edu</a></p>
+                  </div>
+                </div>
+
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="#" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">FACEBOOK</span></a>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-default btn-lg"><i class="fa fa-child fa-fw"></i> <span class="network-name">WIN Network</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Map Section -->
+    <div id="map">
+      <iframe width="100%" height="300" frameborder="0" style="border:0"
+     src="https://www.google.com/maps/embed/v1/place?q=148%20N%20Breese%20Terrace%2C%20Madison%2C%20WI%2C%20United%20States&key=AIzaSyD9BIQXNjZN61o87Y-GLdTcDgU4DiT0Y68 " allowfullscreen></iframe>
+    </div>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; 2016 Triangle Fraternity - Univeristy of Wisconsin, Madison</p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="res/jquery/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="res/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/main.js"></script>
+
+</body>
 
 </html>
-			
