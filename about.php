38c8d032156756c466eb218b959fdc00a652bbdd
Cigarent <xiaojunh1996@hotmail.com>
2016-08-09 17:25:33 -0500

    finish structure of about page

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("includes.php");
+      printHead();?>
+
+<body id="page-top">
+
+    <!-- Navigation -->
+    <?php include("header.php");
+          printNav(1);?>
+
+    <div class="intro about-intro">
+    </div>
+
+    <div class="container">
+      <div  class="well">
+
+        <!-- History -->
+         <div class="row">
+           <div class="col-lg-8">
+             <h3 style="margin-bottom: 10px">
+               About Us
+             </h3>
+             <p>
+                Triangle Fraternity (EST 1907), is a social fraternity exclusively for Engineers, Architects and Scientists. Our purpose is to develop balanced men who cultivate high moral character, foster lifelong friendships and lead lives of integrity. We offer countless leadership opportunities, avenues for community service, scholarships and much, much more. Triangle, thus, provides a bridge between academic life and a career, as well as a foundation for success in the years after college.
+             </p>
+           </div>
+           <div class="col-lg-4">
+             <img src="images/logo.png">
+           </div>
+         </div>
+
+         <hr>
+
+         <!-- Value -->
+         <div class="row">
+           <div class="col-lg-4">
+            <img src="images/logo.png">
+           </div>
+           <div class="col-lg-8">
+             <h3 style="margin-bottom: 10px">
+               What we offer
+             </h3>
+             <ul>
+               <li>SUCCEED ACADEMICALLY<br>Triangle is serious about scholarship. Preparing yourself for your future career and achieving academic success are your primary goals at the University of Wisconsin-Madison.</li>
+
+               <li>LEARN TO BECOME A LEADER<br>Triangle provides an environment that fosters personal growth and professional success and provides the necessary skills to become an effective leader.</li>
+
+               <li>UTILIZE LOCAL AND NATIONAL JOB CONNECTIONS<br>Triangle's 29 nationwide chapters, 25,000 alumni and more than 100 years of history contribute to our rich traditions. Our national list-serves and message boards allow our alumni to share openings at their companies, in some cases before the jobs are made known to the general public.</li>
+
+               <li>SOCIALIZE<br>Social and athletic participation provides an enhancement to your busy and challenging college curriculum. Triangle is invited and has access to many events, including Homecoming, Greek Week/All Campus Party, formals and more.</li>
+
+               <li>DEVELOP LIFE LONG FRIENDSHIPS<br>Some of your brothers will be your closest friends because you shared experiences, worked together toward common goals and celebrated achievements during your time in school.</li>
+             </li>
+           </div>
+         </div>
+
+         <hr>
+
+         <!-- Wsconsin Chapter -->
+         <div class="row">
+           <div class="col-lg-8">
+             <h3 style="margin-bottom: 10px">
+               The Wisconsin Chapter
+             </h3>
+             <p>
+                The University of Wisconsin Madison Chapter was founded in 1913. We own two houses in North Breese Terrace, located right by Camp Randall Stadium. Since its inception, the Wisconsin Chapter has worked to become leaders and positive impact within the UW Madison community. We actively collaberate with out alumni, Triangle National HQ, the Greek Community, and The College of Engineering to ensure our members receive the most out of their collegiate and Triangle experience.
+             </p>
+           </div>
+           <div class="col-lg-4">
+             <img src="images/logo.png">
+           </div>
+         </div>
+
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <?php printFooter(); ?>
+
+	</body>
+
+</html>
